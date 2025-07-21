2101b41738a3d6aef56398bf69b092ff1b3da8c3
Quang Nhat <truongthuyquangnhat1@gmail.cm>
2025-07-21 17:57:45 +0700

    cors

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://food-analysis-frontend.onrender.com'
+}));
 
 app.use(express.json());
 
